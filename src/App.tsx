fa9d35d52f0afa10be71e6b3f71b3030436ab088
Haorui Zhou <haorui2002@gmail.com>
2025-11-24 23:26:29 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -460,7 +460,7 @@ const Hero = () => {
           </p>
           <div className="flex flex-col sm:flex-row justify-center lg:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
             <a
-              href="https://pecan-demo.0001200.xyz"
+              href="http://pecan-demo.0001200.xyz"
               target="_blank"
               rel="noopener noreferrer"
             >
