b5cf1a063f3fb83ed0114912f308da4ec65f9d00
Haorui Zhou <haorui2002@gmail.com>
2026-02-23 18:57:46 -0500

    Update url

--- src/App.tsx
+++ src/App.tsx
@@ -459,7 +459,7 @@ const Hero = () => {
           </p>
           <div className="flex flex-col sm:flex-row justify-center lg:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
             <a
-              href="https://pecan.westernformularacing.org/"
+              href="https://pecan.westernformularacing.org/dashboard"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -471,7 +471,7 @@ const Hero = () => {
               </button>
             </a>
             <a
-              href="https://pecan.westernformularacing.org/"
+              href="https://github.com/Western-Formula-Racing/daq-radio"
               target="_blank"
               rel="noopener noreferrer"
             >
